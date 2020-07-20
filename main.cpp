255f1e1f5007360c7f1381a7f1c5e819548d4bb4
gtrman97 <andrew.killian@g.austincc.edu>
2020-07-20 13:05:41 -0500

    made changes

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@
 int main(){
 
     Car car1("Toyota", "Corolla", "4Y1SL65848Z411439", 2010); 
-    cout << car1.getMake() << endl; 
-    cout << car1.getVin(); 
+    cout << car1.getYear() << " " << car1.getMake() << " " << car1.getModel() << endl; 
 
     return 0; 
 }
