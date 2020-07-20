a101800117f73d055206a012647ed16289a9d7aa
gtrman97 <andrew.killian@g.austincc.edu>
2020-07-20 12:37:54 -0500

    wrote addCar

--- CarCollection.cpp
+++ CarCollection.cpp
@@ -1,14 +1,21 @@
 #include "CarCollection.h"
 
 CarCollection::CarCollection(){
+    
+    headPtr = nullptr; 
 
 }
 CarCollection::addCar(Car newCar){
+
+    CarNode * NewNode = new CarNode; 
+    NewNode->CarData = newCar; 
+    NewNode->next = headPtr;
+    headPtr = NewNode; 
 
 }
 CarCollection::displayCar(string vin){
 
 }
 CarCollection::removeCar(string vin){
-    
+
 }
